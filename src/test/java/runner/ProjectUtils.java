28e9b898d7ee3fae901126ecc4857b9e25165e4e
Maria Zasypkina <maria.zasypkina@gmail.com>
2022-06-23 22:07:44 -0600

    ICF Website Selenium Testing

---
+++ src/test/java/runner/ProjectUtils.java
@@ -0,0 +1,10 @@
+package runner;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ProjectUtils {
+
+}
